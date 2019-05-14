f2edb72bd015020ca796d0447ca1381109940e08
Peter Sigurdson <Peter.Sigurdson@Tor.Loc>
2019-05-14 14:42:54 -0400

    second commit

---
+++ C099999/C099999/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C099999
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Orange
+    {
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while ()
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
+        }
+
+    }
+}
